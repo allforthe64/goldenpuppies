20fce90948c81032b4d27b168cfb0d1ec88da888
William Rainey <williamhrainey@gmail.com>
2023-03-13 13:06:33 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function SendMail() {
+    let params = {
+        from_name: document.getElementById('user_name').value,
+        sender_address: document.getElementById('user_email').value,
+        message: document.getElementById('message').value,
+        to_name: 'Sarah'
+    }
+    emailjs.send('service_rsgd5xv', 'template_0a5nuzs', params).then(function (res) {
+        alert('Success' + res.status)
+    })
+}
