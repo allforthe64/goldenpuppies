cda0d1137e6e0bd95520702c5ef8701e14f9bab4
allforthe64 <williamhrainey@gmail.com>
2024-10-29 13:42:33 -0700

    updated copy

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function SendMail() {
-    let params = {
-        from_name: document.getElementById('user_name').value,
-        sender_address: document.getElementById('user_email').value,
-        message: document.getElementById('message').value,
-        to_name: 'Sarah'
+    try {
+        let params = {
+            from_name: document.getElementById('user_name').value,
+            sender_address: document.getElementById('user_email').value,
+            message: document.getElementById('message').value,
+            to_name: 'Sarah'
+        }
+        emailjs.send('service_rsgd5xv', 'template_0a5nuzs', params).then(function (res) {
+            alert('Success' + res.status)
+        })
+    } catch (err) {
+        alert(err)
     }
-    emailjs.send('service_rsgd5xv', 'template_0a5nuzs', params).then(function (res) {
-        alert('Success' + res.status)
-    })
 }
